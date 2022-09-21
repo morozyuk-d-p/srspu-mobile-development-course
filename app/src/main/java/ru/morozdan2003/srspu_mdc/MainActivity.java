0b9b3de38dd0f324ce0c8e4471490b7c44a370f2
Student <Student@IIST.local>
2022-09-21 16:29:03 +0300

    Second lab

--- app/src/main/java/ru/morozdan2003/srspu_mdc/MainActivity.java
+++ app/src/main/java/ru/morozdan2003/srspu_mdc/MainActivity.java
@@ -2,8 +2,12 @@ package ru.morozdan2003.srspu_mdc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,12 +21,27 @@ public class MainActivity extends AppCompatActivity {
         // Import message string in variable
         String message = getResources().getString(R.string.message);
 
-        // Define programmatic objects for activity views
-        TextView textView = (TextView) findViewById(R.id.message);
-        EditText textField = (EditText) findViewById(R.id.editTextTextPersonName2);
+        // Integer for counter for red button
+        final Integer[] redButtonCounter = {0};
 
-        // Replace contents of defined views with message string
-        textView.setText(message);
-        textField.setText(message);
+        // Define programmatic objects for activity views
+        TextView textView = (TextView) findViewById(R.id.textView);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        ImageButton redButton = (ImageButton) findViewById(R.id.redButton);
+
+        // Setup event listeners for buttons
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                textView.setText(text);
+            }
+        });
+        redButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                redButtonCounter[0]++;
+                textView.setText("Кнопка нажата " + redButtonCounter[0].toString() + " раз");
+            }
+        });
     }
 }
